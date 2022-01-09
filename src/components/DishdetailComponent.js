84b9b69a4504ea2c3081247af17e3779a9205fbf
dmitritruf <gttim2@gmail.com>
2022-01-09 22:01:50 +0530

    Assignment1 Corrected

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,49 +1,67 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle
+} from 'reactstrap';
 class DishDetail extends Component {
 
     constructor(props) {
         super(props);
 
         this.state = {
-           
+
         }
     }
 
-    render(){
-        const dish = this.props.dishes;
-        const comments = this.props.dishes.comments.map((comment) => {
+    getComments(dish){
+        if(dish == null)
             return (
-              <p>{comment.comment}<br/>--{comment.author}, {comment.date}</p>
-            );
-        });
+                <div></div>
+            )
+        const comments = dish.comments.map((comment) => {
+                return (
+                    <p>{comment.comment}<br />--{comment.author}, {comment.date}</p>
+                );
+            });
+
+        return comments;
+    }
+    render() {
+        const dish = this.props.dishes;
+
+        
+        // const comments = dish.comments.map((comment) => {
+        //     return (
+        //         <p>{comment.comment}<br />--{comment.author}, {comment.date}</p>
+        //     );
+        // });
         if (dish != null)
-            return(
-                <div className="row">
-                    <div  className="col-12 col-md-5 m-1">
+            return (
+                <div key={dish.id} className="row">
+                    <div className="col-12 col-md-5 m-1">
                         <Card>
                             <CardImg top src={dish.image} alt={dish.name} />
                             <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
                             </CardBody>
                         </Card>
-                    </div> 
-                    <div  className="col-12 col-md-5 m-1">
+                    </div>
+                    <div key={dish.id} className="col-12 col-md-5 m-1">
                         <h1>Comments</h1>
-                        {comments}
+                        {this.getComments(dish)}
+                        {/* <p>{dish.comments.comment}<br />--{dish.comments.author}, {dish.comments.date}</p> */}
                     </div>
 
-               
-                </div>           
+
+                </div>
             )
-        else{
-            return(
-                <div></div>
-            )
-        }
-       
+
+        return (
+            <div></div>
+        );
+
+
     }
 }
 export default DishDetail;
