d0f6c68757ed9e7bacf2e3aa2c00846da2874f52
dmitritruf <gttim2@gmail.com>
2022-02-25 11:35:16 +0530

    React Router with path

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-function GetComments({ dish }) {
-    if (dish == null) return <div></div>;
-    const comments = dish.comments.map((comment) => {
+function GetComments({comments}) {
+    if (comments == null) return <div></div>;
+    const commentss = comments.map((comment) => {
         return (
             <p>
                 {comment.comment}
@@ -11,9 +13,9 @@ function GetComments({ dish }) {
                 --{comment.author}, {comment.date}
             </p>
         );
-    });
+   });
 
-    return comments;
+    return commentss;
 }
 
 function RenderDish({ dish }) {
@@ -34,13 +36,23 @@ const DishDetail = (props) => {
     if (props.dish != null)
         return (
             <div class="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
                 <div key={props.dish.id} className="row">
                     <div className="col-12 col-md-5 m-1">
                         <RenderDish dish={props.dish}></RenderDish>
                     </div>
                     <div key={props.dish.id} className="col-12 col-md-5 m-1">
                         <h1>Comments</h1>
-                        <GetComments dish={props.dish}></GetComments>
+                        <GetComments comments={props.comments}></GetComments>
                         {/* <p>{dish.comments.comment}<br />--{dish.comments.author}, {dish.comments.date}</p> */}
                     </div>
                 </div>
